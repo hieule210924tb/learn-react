9154a2cbb2331f4d1c272a02eb2f97c9c16b5c78
hieule <manu210924@gmail.com>
2025-02-24 16:26:45 +0700

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import { useState , useCallback} from "react";
+import { useState , useMemo, useRef} from "react";
 import Content from './Content'
 
 function App() {
-  // const [show, setShow] = useState(false)
-  const [count, setCount] = useState(60)
-    
-    const handleIncrease =useCallback(()=>{
-        setCount(prevCount => prevCount+1)
-    },[])
- 
-   
-    return (
-      <div style={{padding:'30px'}}>
-          <Content onIncrease ={handleIncrease} />
-          <h1>{count}</h1>
-        </div>
-    );
-  // return (
-  //   <div className="App" style={{padding:20}}>
-  //     <button onClick={()=> setShow(!show)}>Togger</button>
-  //     {show && <Content/>}
-  //   </div>
-  // );
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [products, setProducts] = useState([])
+
+  const nameRef = useRef() // thêm useRef() để khi add xong nó tự focus vào thẻ input
+  const handleSubmit = () =>{
+      setProducts([...products,{
+        name,
+        price: parseInt(price) // đổi sang dạng số
+      }])
+      setName('')
+      setPrice('')
+      nameRef.current.focus()
+  }
+  
+   const total = useMemo(()=>{
+      const result=  products.reduce((result, prod)=>{
+        return  result + prod.price
+    }
+    ,0)
+    return result
+   },[products])
+
+
+  return(
+    < div style={{padding:'30px'}}>
+       <input
+         ref={nameRef}
+         value={name}
+         placeholder="Nhập tên sản phẩm"
+         onChange={(e) => setName(e.target.value)}
+       />
+       <br />
+       <input
+        value={price}
+        placeholder="Nhập giá sản phẩm"
+         onChange={(e) => setPrice(e.target.value)}
+       />
+        <br />
+        Total: {total}
+        <br />
+       <button 
+         onClick={handleSubmit}
+       >Add</button>
+       <ul>
+       {
+        products.map((product, index) =>(
+          <li key={index}>{product.name} - {product.price}</li>
+        ))
+       }
+       </ul>
+   </div>
+  )
 }
 
 export default App;
